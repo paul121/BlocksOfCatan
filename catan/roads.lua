e40286255a944e33e8101f32ea155e132a0fde64
paul121 <paul.weidner@gmail.com>
2016-11-09 22:40:21 -0800

    Add road to settlement on road claim, templated.

--- catan/roads.lua
+++ catan/roads.lua
@@ -4,37 +4,32 @@ local convertToRoad = function(pos)
   worldedit.set(pos, pos, "catan:road_red")
 end
 
-local nodeIsRoad = function(pos)
+local getNodeType = function(pos)
   local node = minetest.get_node(pos)
-  if node.name == "catan:road_default" then
-    return true
-  else
-    return false
-  end
+  return node.name
+end
+
+local addRoadToSettlement = function(pos)
+  minetest.chat_send_all("Adding a road to settlement.")
 end
 
 local searchForRoad = nil
+
+local testLocation = function(pos)
+  local nodeType = getNodeType(pos)
+  if nodeType == "catan:road_default" then
+    convertToRoad(pos)
+    searchForRoad(pos)
+  elseif nodeType == "catan:settlement_location" then
+    addRoadToSettlement(pos)
+  end
+end
+
 searchForRoad = function(pos)
-  local testpos = posOffset(1, 0, 0, pos)
-  if nodeIsRoad(testpos) then
-    convertToRoad(testpos)
-    searchForRoad(testpos)
-  end
-  testpos = posOffset(-1, 0, 0, pos)
-  if nodeIsRoad(testpos) then
-    convertToRoad(testpos)
-    searchForRoad(testpos)
-  end
-  testpos = posOffset(0, 0, 1, pos)
-  if nodeIsRoad(testpos) then
-    convertToRoad(testpos)
-    searchForRoad(testpos)
-  end
-  testpos = posOffset(0, 0, -1, pos)
-  if nodeIsRoad(testpos) then
-    convertToRoad(testpos)
-    searchForRoad(testpos)
-  end
+  testLocation(posOffset(1, 0, 0, pos))
+  testLocation(posOffset(-1, 0, 0, pos))
+  testLocation(posOffset(0, 0, 1, pos))
+  testLocation(posOffset(0, 0, -1, pos))
 end
 
 local buildRoad = function(player, pos)
@@ -45,15 +40,15 @@ end
 local isRoadLocation = function(pos)
   local centerPos = catan_local.boardsettings.pos
   local differenceVector = {x = centerPos.x - pos.x, y = centerPos.y - pos.y, z = centerPos.z - pos.z}
-  minetest.chat_send_all("Checking if pos is a road...")
+  --minetest.chat_send_all("Checking if pos is a road...")
   if ((differenceVector.z % 8 == 0) and ((differenceVector.x % 14 == 1) or (differenceVector.x % 14 == 13))) or
      ((differenceVector.z % 4 == 0) and ((differenceVector.x % 7 == 1) or (differenceVector.x % 7 == 6))) or
      ((differenceVector.z % 3 == 0) and (differenceVector.x % 7 == 0)) then
 
-    minetest.chat_send_all("Node placed on correct space!")
+    --minetest.chat_send_all("Node placed on correct space!")
     return true
   else
-    minetest.chat_send_all("Not a road...--> "..differenceVector.z % 8)
+    --minetest.chat_send_all("Not a road...--> "..differenceVector.z % 8)
     return false
   end
 end
