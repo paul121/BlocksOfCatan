284191cac3cab5a053eecb9c3908dad7de917586
paul121 <paul.weidner@gmail.com>
2016-11-29 19:25:32 -0800

    Players can build a settlement, based on the players color

--- catan/settlements.lua
+++ catan/settlements.lua
@@ -11,19 +11,27 @@ local buildSettlement = function(player, pos)
   local name = player:get_player_name()
   local board = boardSetup.getBoard()
   local catan_player = board.playerMap[name].player
-
-  for tileNum = 1, #board.tiles do
-    for settleNum = 1, #board.tiles[tileNum].settlements do
-      local settlement = board.tiles[tileNum].settlements[settleNum].pos
-      if pos == settlement.pos then
-        settlement.owner = catan_player
-        minetest.chat_send_all("Found a settlment")
+  if catan_player then
+    if catan_player.color then
+      for tileNum = 1, #board.tiles do
+        for settleNum = 1, #board.tiles[tileNum].settlements do
+          local settlement = board.tiles[tileNum].settlements[settleNum]
+          if minetest.pos_to_string(util.posOffset(0, -1, 0, pos)) == minetest.pos_to_string(settlement.pos) then
+            settlement.owner = catan_player
+            minetest.chat_send_all("Found a settlment")
+          end
+        end
       end
+      worldedit.set(pos, util.posOffset(0, 1, 0, pos), "catan:settlement_"..catan_player.color)
+      boardSetup.saveBoard(board)
+    else
+      return "ERROR: Player has not set a color."
     end
+  else
+    return "ERROR: Player is not in the board."
   end
 
-  worldedit.set(pos, util.posOffset(0, 1, 0, pos), "catan:settlement_"..catan_player.color)
-  boardSetup.saveBoard(board)
+
 end
 
 local isSettlementLocation = function (pos)
@@ -37,7 +45,10 @@ end
 
 local onSettlementBuilderPlace = function(player, pos)
   if isSettlementLocation(pos) then
-    buildSettlement(player, pos)
+    local error = buildSettlement(player, pos)
+    if error then
+      minetest.chat_send_all(error)
+    end
   end
 end
 
